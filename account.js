7ce33cd003308417a050097582979d816430d53b
zehradeveloper <139633876+zehradeveloper@users.noreply.github.com>
2024-11-03 18:25:55 +0300

    .

--- account.js
+++ account.js
@@ -24,30 +24,4 @@ document.getElementById('registerForm').addEventListener('submit', function(even
 
     // Formu temizle
     document.getElementById('registerForm').reset();
-});document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
-
-    // Şifre doğrulama
-    if (password !== confirmPassword) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Hata',
-            text: 'Şifreler eşleşmiyor!'
-        });
-        return;
-    }
-
-    Swal.fire({
-        icon: 'success',
-        title: 'Başarılı',
-        text: 'Kayıt işlemi başarılı!'
-    });
-
-    // Formu temizle
-    document.getElementById('registerForm').reset();
 });
